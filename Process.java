7e41197f89618ecad4c05cc09420c05fec3a11ad
Tanmay-Tripathi <iamtanmay.0101@gmail.com>
2024-10-31 11:19:46 +0530

    Process class added

---
+++ Process.java
@@ -0,0 +1,48 @@
+public class Process {
+    private int id;
+    private int arrivalTime;
+    private int burstTime;
+    private int priority;
+    private int waitingTime;
+    private int turnaroundTime;
+
+    public Process(int id, int arrivalTime, int burstTime, int priority) {
+        this.id = id;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.priority = priority;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public int getWaitingTime() {
+        return waitingTime;
+    }
+
+    public int getTurnaroundTime() {
+        return turnaroundTime;
+    }
+
+    public void setWaitingTime(int waitingTime) {
+        this.waitingTime = waitingTime;
+    }
+
+    public void setTurnaroundTime(int turnaroundTime) {
+        this.turnaroundTime = turnaroundTime;
+    }
+}
